8461b50b81d2870cc36107d5d134346ec08cd5cc
anugasrikarreddy <64678472+anugasrikarreddy@users.noreply.github.com>
2024-03-14 18:04:09 -0400

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -34,8 +34,8 @@ DEBUG = config('DEBUG', cast=bool)
 # DEBUG = True
 
 
-ALLOWED_HOSTS = ['bachisrikar-dev-xsee.4.us-1.fl0.io']
-CSRF_TRUSTED_ORIGINS =['https://bachisrikar-dev-xsee.4.us-1.fl0.io']
+ALLOWED_HOSTS = ['greatkart-dev-jkfa.4.us-1.fl0.io']
+CSRF_TRUSTED_ORIGINS =['https://greatkart-dev-jkfa.4.us-1.fl0.io']
 
 # Application definition
 
